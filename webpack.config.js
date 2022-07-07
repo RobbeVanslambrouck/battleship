239eb5926b72e1c36b2f98370434b5f792c1d4aa
Robbe Vanslambrouck <robbevsb.dev@gmail.com>
2022-07-07 23:13:50 +0200

    improving dev setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,57 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
 
 module.exports = {
-  mode: "development",
-  entry: "./src/index.js",
-  devtool: "inline-source-map",
-  devServer: {
-    static: "./dist",
+  mode: 'development',
+  entry: {
+    bundle: path.resolve(__dirname, 'src/index.js'),
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "battleship",
-    }),
-  ],
   output: {
-    filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash].js',
+    assetModuleFilename: '[name][ext]',
     clean: true,
   },
-  optimization: {
-    runtimeChunk: "single",
+  devtool: 'source-map',
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
   },
   module: {
     rules: [
       {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
       },
       {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: "asset/resource",
+        test: /\.(png|jpg|jpeg|svg|gif)$/i,
+        type: 'asset/resourse',
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'battleship',
+      filename: 'index.html',
+      template: 'src/template.html',
+    }),
+    new ESLintPlugin(),
+  ],
 };

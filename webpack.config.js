928a3aba62bd1e5743a1cb73f1d0eba54ee8abb3
Robbe Vanslambrouck <robbevsb.dev@gmail.com>
2022-07-09 21:20:56 +0200

    added sass loaders

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,11 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/,
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.css/i,
         use: ['style-loader', 'css-loader'],
       },
       {

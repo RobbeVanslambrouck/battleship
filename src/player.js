9a1271cd6751d8c57faf1aafca3a01689d90be2c
Robbe Vanslambrouck <robbevsb.dev@gmail.com>
2022-07-12 20:43:51 +0200

    added ai turns

--- src/player.js
+++ src/player.js
@@ -12,11 +12,125 @@ const Player = (name) => {
     gameboard = NewBoard;
   };
 
-  const turn = (x, y, enemy) => {
-    enemy.getGameboard().receiveAttack(x, y);
+  const turn = (x, y, enemy) => enemy.getGameboard().receiveAttack(x, y);
+
+  const AIRandomTurn = (enemy) => {
+    const attackable = [];
+    const enemyBoard = enemy.getGameboard().getBoard();
+    for (let i = 0; i < enemyBoard.length; i += 1) {
+      for (let j = 0; j < enemyBoard[i].length; j += 1) {
+        if (enemyBoard[i][j] !== 'h' && enemyBoard[i][j] !== 'm') {
+          attackable.push({ x: j, y: i });
+        }
+      }
+    }
+    const i = Math.round(Math.random() * (attackable.length - 1));
+    enemy.getGameboard().receiveAttack(attackable[i].x, attackable[i].y);
   };
 
-  return { getName, SetName, getGameboard, setGameboard, turn };
+  const getCellvalue = (x, y, board) => {
+    let cellValue = 0;
+    if (y < board.length - 1) {
+      if (board[y + 1][x] === 'h') cellValue += 4;
+      if (board[y + 1][x] === 'm') cellValue -= 1;
+    }
+    if (y > 0) {
+      if (board[y - 1][x] === 'h') cellValue += 4;
+      if (board[y - 1][x] === 'm') cellValue -= 1;
+    }
+    if (x < board.length - 1) {
+      if (board[y][x + 1] === 'h') cellValue += 4;
+      if (board[y][x + 1] === 'm') cellValue -= 1;
+    }
+    if (x > 0) {
+      if (board[y][x - 1] === 'h') cellValue += 4;
+      if (board[y][x - 1] === 'm') cellValue -= 1;
+    }
+    return cellValue;
+  };
+
+  const AISmartTurn = (enemy) => {
+    let attackTable = [];
+    let maxCellValue = -Infinity;
+    const enemyBoard = enemy.getGameboard().getBoard();
+    for (let i = 0; i < enemyBoard.length; i += 1) {
+      for (let j = 0; j < enemyBoard[i].length; j += 1) {
+        if (enemyBoard[i][j] !== 'h' && enemyBoard[i][j] !== 'm') {
+          const cellValue = getCellvalue(j, i, enemyBoard);
+          if (cellValue === maxCellValue) {
+            attackTable.push({ x: j, y: i, cellValue });
+          }
+
+          if (cellValue > maxCellValue) {
+            maxCellValue = cellValue;
+            attackTable = [];
+            attackTable.push({ x: j, y: i, cellValue });
+          }
+        }
+      }
+    }
+    console.log(attackTable.length);
+    const i = Math.round(Math.random() * (attackTable.length - 1));
+    enemy.getGameboard().receiveAttack(attackTable[i].x, attackTable[i].y);
+  };
+
+  const AIImpossibleTurn = (enemy) => {
+    const attackTable = [];
+    const enemyBoard = enemy.getGameboard().getBoard();
+    for (let i = 0; i < enemyBoard.length; i += 1) {
+      for (let j = 0; j < enemyBoard[i].length; j += 1) {
+        if (
+          enemyBoard[i][j] !== 'h' &&
+          enemyBoard[i][j] !== 'm' &&
+          enemyBoard[i][j] !== 'w'
+        ) {
+          attackTable.push({ x: j, y: i });
+        }
+      }
+    }
+    if (attackTable.length === 0) {
+      AIRandomTurn(enemy);
+      return;
+    }
+    console.log(attackTable.length);
+    const i = Math.round(Math.random() * (attackTable.length - 1));
+    enemy.getGameboard().receiveAttack(attackTable[i].x, attackTable[i].y);
+  };
+
+  const AIInstaKillTurn = (enemy) => {
+    const attackTable = [];
+    const enemyBoard = enemy.getGameboard().getBoard();
+    for (let i = 0; i < enemyBoard.length; i += 1) {
+      for (let j = 0; j < enemyBoard[i].length; j += 1) {
+        if (
+          enemyBoard[i][j] !== 'h' &&
+          enemyBoard[i][j] !== 'm' &&
+          enemyBoard[i][j] !== 'w'
+        ) {
+          attackTable.push({ x: j, y: i });
+        }
+      }
+    }
+    if (attackTable.length === 0) {
+      AIRandomTurn(enemy);
+      return;
+    }
+    attackTable.forEach((attack) => {
+      enemy.getGameboard().receiveAttack(attack.x, attack.y);
+    });
+  };
+
+  return {
+    getName,
+    SetName,
+    getGameboard,
+    setGameboard,
+    turn,
+    AIRandomTurn,
+    AISmartTurn,
+    AIImpossibleTurn,
+    AIInstaKillTurn,
+  };
 };
 
 export default Player;
--- src/player.test.js
+++ src/player.test.js
@@ -1,5 +1,6 @@
 import Player from './player';
 import Gameboard from './gameboard';
+import Ship from './ship';
 
 test('get/set name', () => {
   const player = Player('name');
@@ -17,9 +18,48 @@ test('get/set gameboard', () => {
 
 test('turn', () => {
   const player = Player('p1');
+  const ship = Ship(2);
   const enemy = Player('p2');
+  enemy.getGameboard().placeShip(ship, 0, 1, 'h');
   const board = Gameboard(10);
+  board.placeShip(ship, 0, 1, 'h');
   board.receiveAttack(0, 0);
-  player.turn(0, 0, enemy);
+  board.receiveAttack(0, 1);
+  expect(player.turn(0, 0, enemy)).toBe('miss');
+  expect(player.turn(0, 0, enemy)).toBe('already attacked');
+  expect(player.turn(0, 1, enemy)).toBe('hit');
+  expect(player.turn(0, 1, enemy)).toBe('already attacked');
+  expect(enemy.getGameboard().getBoard()).toStrictEqual(board.getBoard());
+});
+
+test('AIRandomTurn', () => {
+  const player = Player('p1');
+  const enemy = Player('p2');
+  enemy.setGameboard(Gameboard(2));
+  const board = Gameboard(2);
+  board.receiveAttack(0, 0);
+  board.receiveAttack(0, 1);
+  board.receiveAttack(1, 0);
+  board.receiveAttack(1, 1);
+  player.AIRandomTurn(enemy);
+  player.AIRandomTurn(enemy);
+  player.AIRandomTurn(enemy);
+  player.AIRandomTurn(enemy);
+  expect(enemy.getGameboard().getBoard()).toStrictEqual(board.getBoard());
+});
+
+test('AISmartTurn', () => {
+  const player = Player('p1');
+  const enemy = Player('p2');
+  enemy.setGameboard(Gameboard(2));
+  const board = Gameboard(2);
+  board.receiveAttack(0, 0);
+  board.receiveAttack(0, 1);
+  board.receiveAttack(1, 0);
+  board.receiveAttack(1, 1);
+  player.AISmartTurn(enemy);
+  player.AISmartTurn(enemy);
+  player.AISmartTurn(enemy);
+  player.AISmartTurn(enemy);
   expect(enemy.getGameboard().getBoard()).toStrictEqual(board.getBoard());
 });

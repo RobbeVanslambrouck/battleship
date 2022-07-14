6614240c16abba64157540156ae49b364bad611a
Robbe Vanslambrouck <robbevsb.dev@gmail.com>
2022-07-14 21:05:22 +0200

    added gamemodes and cleaned dom manipulation functions

--- src/domElements.js
+++ src/domElements.js
@@ -1,6 +1,7 @@
 import PubSub from 'pubsub-js';
 
 const DomElements = (() => {
+  const game = document.querySelector('.game');
   const createBoard = (board, showShips = true, subTopic = null) => {
     const boardElement = document.createElement('div');
     boardElement.className = 'board';
@@ -9,53 +10,70 @@ const DomElements = (() => {
       row.className = 'row';
       row.id = `row ${i}`;
       for (let j = 0; j < board[i].length; j += 1) {
-        const cell = document.createElement('div');
-        cell.classList.add('cell');
-        cell.id = `${j} ${i}`;
+        const Tile = document.createElement('div');
+        Tile.classList.add('tile');
+        Tile.id = `${j} ${i}`;
         if (subTopic) {
-          cell.onclick = () => {
+          Tile.onclick = () => {
             PubSub.publish(subTopic, { x: j, y: i });
           };
         }
         switch (board[i][j]) {
-          case 'm':
-            cell.classList.add('pin');
-            cell.classList.add('miss');
-            break;
           case 'w':
-            cell.classList.add('water');
+            Tile.classList.add('water');
+            break;
+          case 'm':
+            Tile.classList.add('pin');
+            Tile.classList.add('miss');
             break;
           case 'h':
-            cell.classList.add('pin');
-            cell.classList.add('hit');
+            Tile.classList.add('pin');
+            Tile.classList.add('hit');
+            break;
+          case 's':
+            Tile.classList.add('pin');
+            Tile.classList.add('hit');
+            Tile.classList.add('sunk');
             break;
           default:
             if (showShips) {
-              cell.classList.add('ship');
+              Tile.classList.add('ship');
               break;
             }
-            cell.classList.add('water');
+            Tile.classList.add('water');
             break;
         }
-        row.append(cell);
+        row.append(Tile);
       }
       boardElement.append(row);
     }
     return boardElement;
   };
 
-  const renderPlayerBoard = (board) => {
-    const playerBoard = document.querySelector('.playerBoard');
-    playerBoard.innerHTML = '';
-    playerBoard.append(createBoard(board));
+  const renderBoard = (board, playerType, id, clickPub = null) => {
+    game.querySelector('.game');
+    const divBoard = document.createElement('div');
+    divBoard.id = `board-${id}`;
+    divBoard.classList.add(playerType);
+    const showShips = playerType === 'player';
+    divBoard.append(createBoard(board, showShips, clickPub));
+    game.append(divBoard);
   };
-  const renderEnemyBoard = (board, clickTopic) => {
-    const enemyBoard = document.querySelector('.enemyBoard');
-    enemyBoard.innerHTML = '';
-    enemyBoard.classList.add('enemy');
-    enemyBoard.append(createBoard(board, false, clickTopic));
+
+  const updateBoard = (id, board, clickPub = null) => {
+    game.querySelector('.game');
+    const divBoard = game.querySelector(`#board-${id}`);
+    const showShips = divBoard.classList.contains('player');
+    divBoard.innerHTML = '';
+    divBoard.append(createBoard(board, showShips, clickPub));
   };
-  return { renderPlayerBoard, renderEnemyBoard };
+
+  const clearGame = () => {
+    game.querySelector('.game');
+    game.innerHTML = '';
+  };
+
+  return { renderBoard, updateBoard, clearGame };
 })();
 
 export default DomElements;
--- src/game.js
+++ src/game.js
@@ -4,9 +4,45 @@ import Player from './player';
 import Ship from './ship';
 
 const Game = (() => {
-  const players = [];
+  let players = [];
+  const gameModes = ['CPU_FIGHT', 'CPU_VS_PLAYER', 'PLAYER_VS_PLAYER'];
+  const gameMode = gameModes[1];
 
-  const init = () => {
+  const cpuVsCpu = () => {
+    let gameOver = false;
+    players = [];
+    players[0] = Player('cpu 1');
+    players[1] = Player('cpu 2');
+
+    const playerShips = [Ship(5), Ship(4), Ship(3), Ship(3), Ship(2)];
+
+    for (let i = 0; i < playerShips.length; i += 1) {
+      players[0].getGameboard().placeShipRandomly(playerShips[i]);
+    }
+
+    const enemyShips = [Ship(5), Ship(4), Ship(3), Ship(3), Ship(2)];
+
+    for (let i = 0; i < enemyShips.length; i += 1) {
+      players[1].getGameboard().placeShipRandomly(enemyShips[i]);
+    }
+    DomElements.renderBoard(players[0].getGameboard().getBoard(), 'player', 0);
+    DomElements.renderBoard(players[1].getGameboard().getBoard(), 'player', 1);
+    while (!gameOver) {
+      players[0].AISmartTurn(players[1]);
+      DomElements.updateBoard(0, players[0].getGameboard().getBoard());
+      gameOver = players[0].getGameboard().areAllShipsSunk();
+      if (gameOver) break;
+      players[1].AISmartTurn(players[0]);
+      DomElements.updateBoard(1, players[1].getGameboard().getBoard());
+      gameOver = players[1].getGameboard().areAllShipsSunk();
+    }
+    console.log('game over');
+  };
+
+  const cpuVsPlayer = () => {
+    const PLAYER = 0;
+    const ENEMY = 1;
+    const ATTACK_TOPIC = 'attack';
     players[0] = Player('player');
     players[1] = Player('cpu');
 
@@ -21,36 +57,17 @@ const Game = (() => {
     for (let i = 0; i < enemyShips.length; i += 1) {
       players[1].getGameboard().placeShipRandomly(enemyShips[i]);
     }
-  };
-
-  const startGame = () => {
-    init();
-    const PLAYER = 0;
-    const ENEMY = 1;
-    const ATTACK_TOPIC = 'attack';
-    const updateEnemyBoard = () => {
-      DomElements.renderEnemyBoard(
-        players[ENEMY].getGameboard().getBoard(),
-        ATTACK_TOPIC
-      );
-    };
-    const updatePlayerBoard = () => {
-      DomElements.renderPlayerBoard(players[PLAYER].getGameboard().getBoard());
-    };
-    updateEnemyBoard();
-    updatePlayerBoard();
-
-    const enemyAttack = (aiLevel) => {
-      if (aiLevel === "can't lose") {
-        players[ENEMY].AIRandomTurn(players[PLAYER]);
-      } else if (aiLevel === 'easy') {
-        players[ENEMY].AISmartTurn(players[PLAYER]);
-      } else if (aiLevel === 'impossible') {
-        players[ENEMY].AIImpossibleTurn(players[PLAYER]);
-      } else if (aiLevel === 'instaKill') {
-        players[ENEMY].AIInstaKillTurn(players[PLAYER]);
-      }
-    };
+    DomElements.renderBoard(
+      players[PLAYER].getGameboard().getBoard(),
+      'player',
+      0
+    );
+    DomElements.renderBoard(
+      players[ENEMY].getGameboard().getBoard(),
+      'enemy',
+      1,
+      ATTACK_TOPIC
+    );
 
     let token = '';
     const attackSub = (msg, data) => {
@@ -60,14 +77,18 @@ const Game = (() => {
         token = PubSub.subscribe(ATTACK_TOPIC, attackSub);
         return;
       }
-      updateEnemyBoard();
+      DomElements.updateBoard(
+        1,
+        players[ENEMY].getGameboard().getBoard(),
+        ATTACK_TOPIC
+      );
       const playerWon = players[ENEMY].getGameboard().areAllShipsSunk();
       if (playerWon) {
         console.log(`game over: ${players[PLAYER].getName()} won`);
         return;
       }
-      enemyAttack('easy');
-      updatePlayerBoard();
+      players[ENEMY].AISmartTurn(players[PLAYER]);
+      DomElements.updateBoard(0, players[PLAYER].getGameboard().getBoard());
       const EnemyWon = players[PLAYER].getGameboard().areAllShipsSunk();
       if (EnemyWon) {
         console.log(`game over: ${players[ENEMY].getName()} won`);
@@ -77,6 +98,26 @@ const Game = (() => {
     };
 
     token = PubSub.subscribe(ATTACK_TOPIC, attackSub);
+  };
+
+  const PlayerVsPlayer = () => {
+    console.log('not yet implemented');
+  };
+
+  const startGame = () => {
+    if (gameMode === 'CPU_FIGHT') {
+      cpuVsCpu();
+      return;
+    }
+
+    if (gameMode === 'CPU_VS_PLAYER') {
+      cpuVsPlayer();
+      return;
+    }
+
+    if (gameMode === 'PLAYER_VS_PLAYER') {
+      PlayerVsPlayer();
+    }
   };
   return { startGame };
 })();

642487555606fa7657ebb4d7d14f1d9bb9a9f425
Robbe Vanslambrouck <robbevsb.dev@gmail.com>
2022-09-06 15:49:04 +0200

    update live demo

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ESLintPlugin = require('eslint-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: {
     bundle: path.resolve(__dirname, 'src/index.js'),
   },
